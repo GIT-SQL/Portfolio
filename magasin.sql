b0d1fe1549e9144ee93d6cc2617684277c604b40
SQuaLe <visquert.lewis@gmail.com>
2023-10-20 16:31:33 +0200

    Valeur introduite dans la table

--- magasin.sql
+++ magasin.sql
@@ -10,3 +10,10 @@ CREATE TABLE Articles (
     prix INT,
     nutriscore VARCHAR(1)
 );
+
+INSERT INTO Articles (id, nom, marque, quantité, prix, nutriscore)
+VALUES
+    (1, "Jus d'orange", 'Joker', 100, 2.99, 'C'),
+    (2, 'Pain complet', 'Grains Bio', 50, 3.49, 'A'),
+    (3, 'Yaourt aux fraises', 'La Laiterie', 75, 1.99, 'B'),
+    (4, 'Pâtes à spaghetti', 'Barilla', 200, 1.79, 'A');
